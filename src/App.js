fb787fa8ae2d9efad29bc5932a5adfdd2bb6571e
zuzKomar <s17301@pjwstk.edu.pl>
2021-10-09 16:17:49 +0200

    added filtering based on Appointment type

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navigation from "./components/fragments/Navigation";
 import ReferralList from "./components/referral/ReferralList";
 import HomePage from "./components/patient/HomePage";
+import AppointmentList from "./components/appointment/AppointmentList";
 
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
+
+
 
 
 function App() {
@@ -17,6 +20,7 @@ function App() {
               <Navigation/>
               <Switch>
                   <Route exact path="/moje_konto" component={HomePage}/>
+                  <Route exact path="/wizyty" component={AppointmentList}/>
                   <Route exact path="/skierowania" component={ReferralList}/>
               </Switch>
           </div>
