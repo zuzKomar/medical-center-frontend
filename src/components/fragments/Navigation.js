0455bd1760fe0d4e6b6d1e56dd63f7f259400d26
zuzKomar <s17301@pjwstk.edu.pl>
2021-10-23 17:38:27 +0200

    added navigation styling

--- src/components/fragments/Navigation.js
+++ src/components/fragments/Navigation.js
@@ -2,19 +2,17 @@ import React from "react";
 import logo from '../../logo.png';
 import {Navbar, Nav, NavDropdown, Container} from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap'
-
+import {BsPerson} from 'react-icons/bs'
 
 function Navigation(){
+    const profileTitle = (<BsPerson size={42}/>);
     return(
             <Navbar bg="light" variant="light" expand="lg" collapseOnSelect="true" sticky="top" className="navContainer">
                 <Container fluid>
                     <Navbar.Brand className="navbar-brand"><img src={logo} /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav" className="navCollapse">
+                    <Navbar.Collapse id="navbarToggleExternalContent" className="navCollapse">
                         <Nav className="navNav">
-                            <LinkContainer to="/moje-konto" activeHref="active">
-                                <Nav.Link exact className="navItemLink">Moje konto</Nav.Link>
-                            </LinkContainer>
                             <LinkContainer to="/wizyty" activeHref="active">
                                 <Nav.Link exact className="navItemLink">Wizyty</Nav.Link>
                             </LinkContainer>
@@ -31,9 +29,14 @@ function Navigation(){
                                 <Nav.Link exact className="navItemLink">Recepty</Nav.Link>
                             </LinkContainer>
                         </Nav>
-                        <NavDropdown className="navItemLanguages" title="Język" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#angielski" >Angielski</NavDropdown.Item>
-                            <NavDropdown.Item href="#polski">Polski</NavDropdown.Item>
+                        <NavDropdown className="navItemLanguages" title={profileTitle} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/moje-konto">Moje konto</NavDropdown.Item>
+                            <NavDropdown title="Język" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#angielski" >Angielski</NavDropdown.Item>
+                                <NavDropdown.Item href="#polski">Polski</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#wyloguj">Wyloguj</NavDropdown.Item>
                         </NavDropdown>
                     </Navbar.Collapse>
                 </Container>
