134b51cd59f441be49939fc1a28be6dba6579c6e
JanRejnowskis18410 <jan.rejnowski@gmail.com>
2021-12-16 12:14:52 +0100

    Finished AppointmentDetailsButtonPanel;

--- src/components/appointment/doctor/AppointmentDetailsButtonPanel.js
+++ src/components/appointment/doctor/AppointmentDetailsButtonPanel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {useHistory} from "react-router-dom";
 
-const AppointmentDetailsButtonPanel = ({appointment}) => {
+const AppointmentDetailsButtonPanel = ({appointment, active}) => {
 
     let history = useHistory();
 
@@ -21,6 +21,12 @@ const AppointmentDetailsButtonPanel = ({appointment}) => {
                 state: appointment
             })
         }
+        else if (button === 'backButton') {
+            history.push({
+                pathname: `/today-visits/${appointment.id}/details`,
+                state: appointment
+            })
+        }
         else {
             history.push({
                 pathname: `/today-visits/${appointment.id}/details/files`,
@@ -31,9 +37,10 @@ const AppointmentDetailsButtonPanel = ({appointment}) => {
 
     return (
         <div className="topBuffer" style={{display: 'flex' ,justifyContent: 'space-between', width:'60%'}}>
-            <button id="visitButton" className="actionButton" onClick={handleClick}>VISITS HISTORY</button>
-            <button id="checkUpButton" className="actionButton" onClick={handleClick}>CHECK-UPS</button>
-            <button id="filesButton" className="actionButton" onClick={handleClick}>PATIENT'S FILES</button>
+            {(window.location.href.indexOf('visits-history') > -1 || window.location.href.indexOf('check-ups') > -1 || window.location.href.indexOf('details/files') > -1) &&  <button id="backButton" className="actionButton" onClick={handleClick}>BACK TO DETAILS</button>}
+            {window.location.href.indexOf('visits-history') < 0 && <button id="visitButton" className="actionButton" onClick={handleClick}>VISITS HISTORY</button> }
+            {window.location.href.indexOf('check-ups') < 0 && <button id="checkUpButton" className="actionButton" onClick={handleClick}>CHECK-UPS</button> }
+            {window.location.href.indexOf('details/files') < 0 && <button id="filesButton" className="actionButton" onClick={handleClick}>PATIENT'S FILES</button> }
         </div>
     )
 }
