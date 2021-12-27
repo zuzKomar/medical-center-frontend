2a0c64d279ebd5d87cf1546c43424c54608d7679
JanRejnowskis18410 <jan.rejnowski@gmail.com>
2021-12-27 19:46:58 +0100

    Quickfixes in index.js;

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Test from "./Test";
 
 import './i18n'
 
 ReactDOM.render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
-        {/*<Test />*/}
         <App />
         {/*<DoctorApp/>*/}
         {/*<LoginApp/>*/}
